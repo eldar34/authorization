ea4fe5d24526aa66f502b797384b1ccf1853c515
eldar <eldar.kerimov@lynkage.ru>
2020-02-07 13:46:55 +0300

    Method for user registration

--- app/CreateReport.php
+++ app/CreateReport.php
@@ -27,4 +27,45 @@ class CreateReport
             }
         }
     }
+
+    public function addRecord($name, $surname, $email, $pass, $filePath)
+    {
+        $response = [];
+        $errors = [];
+        try {
+                $salt = time();
+                $newPassword = md5($pass.$salt);
+                $connection = new Connection();
+                $pdo = $connection->dbConnect();
+                $pdo->beginTransaction();
+                $pdo->exec("INSERT INTO users (name, surname, email, password, file, salt) 
+                VALUES ('$name', '$surname', '$email', '$newPassword', '$filePath', '$salt')");
+                $pdo->commit();
+        } catch (\Exception $e) {
+            $pdo->rollBack();
+
+            if ($e->getCode() == '23000') {
+                array_push($errors, ' already registered');
+                array_push($errors, ' уже зарегестрирован');
+                $response['status'] = 'error';
+                $response['field'] = 'staticEmail';
+                $response['errors'] = $errors;
+                return $response;
+                exit;
+            }
+            //array_push($errors, "Ошибка: " . $e->getMessage());
+            //array_push($errors, "Error");
+            array_push($errors, ' error writing to database');
+            array_push($errors, ' ошибка записи в базу данных');
+            $response['status'] = 'error';
+            $response['field'] = 'staticDB';
+            $response['errors'] = $errors;
+            return $response;
+            exit;
+        }
+        $response['status'] = 'success';
+        $response['field'] = 'staticDB';
+        return $response;
+        exit;
+    }
 }
